024e04f5200a6e13f03f144283f0b96eb30df0ce
Arunkrrish <40736262+Arunkrrish@users.noreply.github.com>
2021-09-07 23:41:24 +0530

    Python DAY 1 Assignment

---
+++ assignment1.py
@@ -0,0 +1,30 @@
+# 1. add two number 
+
+a = 5
+
+b = 10
+
+c = a+b
+
+print ("C is ", c)
+
+# 2. Try to add integer with string and see the output
+
+d = 5
+
+e = "10"
+
+f = d+e
+
+print ("F is ", f)
+
+# 3. Print variable and strings in one line with the help of f string 
+
+name="krrish"
+percentage = 70.5
+
+print (f"{name} got {percentage} in college")
+
+# 4. How to add space and new line in the print statement
+
+print ("This is python"+ "   " +"print \n syntax")
