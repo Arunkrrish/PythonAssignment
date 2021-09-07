a9acce5aadfef1ae0f46db088f2ba5dae43c650b
Arunkrrish <40736262+Arunkrrish@users.noreply.github.com>
2021-09-08 00:01:53 +0530

    Python DAY 2 Assignment

---
+++ assignment2.py
@@ -0,0 +1,18 @@
+# Write a program to find the prime number 
+
+# Enter a number to program and get the output "Prime" or "Not Prime"
+
+num = int(input("Enter a number: "))
+
+flag = False
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
